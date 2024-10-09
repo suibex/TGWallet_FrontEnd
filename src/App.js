7963022ae72d445f02d5f212cfdc3fcd35aeb2f8
nitrodegen <gavrilo.net@gmail.com>
2024-10-10 01:50:05 +0200

    something works

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import './App.css';
 import { TonConnectUIProvider, TonConnectButton,useTonAddress,useTonWallet} from '@tonconnect/ui-react';
 import {useEffect, useState} from 'react'
+import { createAppKit } from '@reown/appkit/react'
 
-export function WalletConnectStuff(){
+import { WagmiProvider } from 'wagmi'
+import { arbitrum, mainnet } from '@reown/appkit/networks'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+const queryClient = new QueryClient()
+
+const projectId = '8683af0ba12075372e6ed1c0844e70cb'
+
+const metadata = {
+  name: 'Geocold',
+  description: 'AppKit Example',
+  url: 'https://suibex.github.io/TGWallet_FrontEnd', // origin must match your domain & subdomain
+  icons: ['https://suibex.github.io/TGWallet_FrontEnd/favicon.ico']
+}
+
+const networks = [mainnet, arbitrum]
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true
+});
+
+export function WalletConnect({children}){
+  /*
+  return (
+    <div>
+      <button class="zmaj">Connect To MetaMask</button>
+    </div>
+  )
+    */return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  </WagmiProvider>
+)
+}
+
+export function ConnectTelegramWallet(){
   const wallet = useTonWallet();   
   const [walletAddress, setWalletAddress] = useState(null); 
 
   useEffect(()=>{
     if(wallet){
-      console.log("Wallet Connected:", wallet);
+      
       setWalletAddress(wallet.account.address);
     } else {
-      console.log("No wallet found");
+     
     }
   },[wallet]);
   console.log(wallet)
@@ -43,11 +93,10 @@ function App() {
  
     <div className="App">
       <header className="App-header">
-
-      <WalletConnectStuff/>
+      
+      <ConnectTelegramWallet/>
       <br></br>
-      <button class="zmaj">Connect To MetaMask</button>
-
+      <WalletConnect/>
       </header>
     
     </div>
